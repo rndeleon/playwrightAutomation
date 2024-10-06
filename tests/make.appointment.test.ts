5e25953d01066d5cbe061d89ebad144a169edf70
raisinsnapink <109150287+rndeleon@users.noreply.github.com>
2024-10-06 11:31:06 +0800

    Add beforeEach for login test

--- tests/make.appointment.test.ts
+++ tests/make.appointment.test.ts
@@ -1,12 +1,27 @@
 import { test, expect } from '@playwright/test';
 
-test('get started link', async ({ page }) => {
+async function login(page:any, username: 'John Doe', password: "ThisIsNotAPassword") {
+  // Fill in the username and password fields
+
+  await page.getByLabel('Username').fill(username);
+  await page.getByLabel('password').fill(password);
+  // Click the login button
+  await page.getByRole('button', { name: 'Login' }).click();
+}
+
+
+test.beforeEach(async ({ page }) => {
   await page.goto('https://katalon-demo-cura.herokuapp.com');
   
   await expect(page).toHaveTitle(/CURA Healthcare Service/);
-  // Click the get started link.  
+
   await page.locator("xpath=//a[text()='Make Appointment']").click();
 
-  // Expects page to have a heading with the name of Installation.
   await expect(page.locator("xpath=//h2[text()='Login']")).toBeVisible();
+});
+
+test('Make Appointment', async ({ page }) => {
+  await login(page, 'John Doe', 'ThisIsNotAPassword');
+
+  await expect(page.locator("xpath=//h2[text()='Make Appointment']")).toBeVisible();
 }); 
